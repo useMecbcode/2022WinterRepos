3a560eab029a361d438b23395b7197df79237018
useMecbcode <lee.wyt.lly@gmail.com>
2022-01-06 13:25:24 +0800

    tests git

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+int add(int a, int b){
+    return a + b;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << add(3, 5) << std::endl;
     return 0;
 }
