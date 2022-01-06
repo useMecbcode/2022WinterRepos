1d1936d588a915d02fb2195e6fd7cac310671a90
useMecbcode <lee.wyt.lly@gmail.com>
2022-01-06 20:31:57 +0800

    python git 测试

--- python/hello/main.py
+++ python/hello/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+class Node(object):
+    def __init__(self, value):
+        self.__init__(value, None)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def __init__(self, value, next):
+        self.data = value
+        self.next = next
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
