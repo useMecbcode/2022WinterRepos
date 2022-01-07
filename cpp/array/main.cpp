0eacd01ed5deb93024e851b8acad3cd20a532503
useMecbcode <lee.wyt.lly@gmail.com>
2022-01-07 10:38:39 +0800

    array

--- cpp/array/main.cpp
+++ cpp/array/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+/**
+ * Array 数组是 增删改查 offset [0...length] 表示地址的偏移
+ */
+template<typename DataType>
+class Array {
+    /**
+     *
+     */
+    using size_t = std::size_t;
+private:
+    DataType data;
+    size_t capacity; // capacity
+    size_t length;  // current length
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
